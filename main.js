71caf126ce58bb9bce63cbe73e9a9d4d344f4cb7
unknown <divine00329@gmail.com>
2024-10-08 06:23:17 +0100

    color changes

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const drawBoard = () => {
     for (let row = 0; row < boardScale; row += sqreScale) {
         for (let col = 0; col < boardScale; col += sqreScale) {
             const newSqr = document.createElement('div');
-            newSqr.style.backgroundColor = (row + col) % 120 === 0 ? '#e3c16f' : '#b88b4a';
+            newSqr.style.backgroundColor = (row + col) % 120 === 0 ? '#d3dee5' : '#7599b1';
             newSqr.style.left = `${col}px`;
             newSqr.style.top = `${row}px`;
             newSqr.style.width = `${sqreScale}px`;
